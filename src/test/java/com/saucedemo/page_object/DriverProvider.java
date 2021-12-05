72891645d0e19be2b91613b624bd8e410416b02a
Dmitriy Zaprutski <zaprutsciy@mail.ru>
2021-12-05 18:03:01 +0300

    Changed DriverProvider

--- src/test/java/com/saucedemo/page_object/DriverProvider.java
+++ src/test/java/com/saucedemo/page_object/DriverProvider.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 import java.util.concurrent.TimeUnit;
 
 public class DriverProvider {
@@ -12,8 +14,10 @@ public class DriverProvider {
 
     public static WebDriver get(){
         if(driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+//            WebDriverManager.chromedriver().setup();
+//            driver = new ChromeDriver();
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
             driver.get("https://www.saucedemo.com/");
         }
